06315414bb42949c6129b95926d52b5ea0a96d61
GuilleTorres <guilletorres81@gmail.com>
2026-08-21 04:13:25 -0300

    Imagenes

--- portfolio-torres/src/components/PortfolioBody.jsx
+++ portfolio-torres/src/components/PortfolioBody.jsx
@@ -1,9 +1,13 @@
 import Hero from './Hero'
-import heroImg from '../assets/hero.png'
 import saltaCodeLogo from '../assets/saltacode-dark.png'
 import ProjectCard from './ProjectCard'
 import JobCard from './JobCard'
 import StackGrid from './StackGrid'
+
+import nutriTurnosImg from "../assets/1.webp";
+import simirImg from "../assets/4.webp";
+import prestockImg from "../assets/7.webp";
+import villaStoreImg from "../assets/8.webp";
 
 function PortfolioBody() {
   const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -31,22 +35,30 @@ function PortfolioBody() {
             <ProjectCard
               title="NutriTurnos"
               description="Sistema de turnos personalizado para consultorios(nutricionista en este caso). Permite gestionar turnos y horarios de atención de manera flexible."
-              image={heroImg}
+              image={nutriTurnosImg}
               buttonLabel="Ver proyecto"
               buttonLink="#contact"
             />
             <ProjectCard
               title="Si.Mi.R"
               description="Es un sistema que permite registrar al personal militar que asiste al comedor diariamente, con el fin de llevar un control de las comidas servidas y los costos asociados."
-              image={heroImg}
+              image={simirImg}
               buttonLabel="Abrir demo"
               buttonLink="#projects"
             />
             <ProjectCard
               title="Prestock"
               description="Es un sistema que gestiona el inventario, y préstamos de elementos de un Centro de Estudiantes. Tambien registra uso de impresiones y entrada y salidas de los encargados de turno"
-              image={heroImg}
-              buttonLink=""
+              image={prestockImg}
+              buttonLabel="Ver proyecto"
+              buttonLink="#contact"
+            />
+            <ProjectCard
+              title="Villa Store"
+              description="E-commerce sencillo para la venta de productos varios. Permite gestionar productos, promociones y pedidos."
+              image={villaStoreImg}
+              buttonLabel="Ver proyecto"
+              buttonLink="#contact"
             />
           </div>
         </div>
